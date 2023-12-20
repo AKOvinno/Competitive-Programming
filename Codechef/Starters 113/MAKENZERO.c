21cc7152dd88e28807dd737b0615858a42feb2a8
Ashfaq Kadir Ovinno <akovinno.official@gmail.com>
2023-12-20 22:24:45 +0600

    codechef codes added

---
+++ Codechef/Starters 113/MAKENZERO.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    while(t--) {
+        int n;
+        scanf("%d", &n);
+        if(n < 3) {
+            printf("NO\n");
+        }
+
+    }
+    return 0;
+}
