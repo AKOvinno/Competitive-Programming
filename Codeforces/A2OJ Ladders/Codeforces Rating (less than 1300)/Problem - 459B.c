e8bb78eba8aa01629f034e102305b971caaf31bf
Ashfaq Kadir Ovinno <akovinno.official@gmail.com>
2024-05-22 22:43:55 +0600

    codechef starters 135 added

--- Codeforces/A2OJ Ladders/Codeforces Rating (less than 1300)/Problem - 459B.c
+++ Codeforces/A2OJ Ladders/Codeforces Rating (less than 1300)/Problem - 459B.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 int main()
 {
-    int n, max = 0, min = 1000000000, max_count = 0, min_count = 0;
-    scanf("%d", &n);
-    int ara[n];
+    long long int n, max = 0, min = 1000000000, max_count = 0, min_count = 0;
+    scanf("%lld", &n);
+    long long int ara[n];
     for(int i = 0; i < n; i++) {
-        scanf("%d", &ara[i]);
+        scanf("%lld", &ara[i]);
         if(ara[i] > max) {
             max = ara[i];
         }
@@ -22,10 +22,13 @@ int main()
             min_count++;
         }
     }
-    if(max_count == min_count) {
-        min_count--;
+    long long int total;
+    if(min == max) {
+        total = (n*(n-1))/2;
     }
-    long long int total = 1ll * max_count * min_count;
+    else {
+        total = max_count * min_count;
+    }
     printf("%d %lld\n", diff, total);
     return 0;
 }
--- Codeforces/A2OJ Ladders/Codeforces Rating (less than 1300)/Problem - 58A.c
+++ Codeforces/A2OJ Ladders/Codeforces Rating (less than 1300)/Problem - 58A.c
@@ -4,28 +4,25 @@ int main()
 {
     char s[101];
     scanf("%s", s);
-    int h_flag = 0, e_flag = 0, l1_flag = 0, l2_flag = 0, o_flag = 0;
+    int count = 0;
     for(int i = 0; i < strlen(s); i++) {
-        if(s[i] == 'h' && h_flag == 0) {
-            if(i == 0) {
-                h_flag = -1;
-            }
+        if(s[i] == 'h' && count == 0) {
+            count++;
         }
-        else if(s[i] == 'e' && e_flag == 0) {
-            e_flag = i;
+        else if(s[i] == 'e' && count == 1) {
+            count++;
         }
-        else if(s[i] == 'l' && l1_flag == 0) {
-            l1_flag = i;
+        else if(s[i] == 'l'  && count == 2) {
+            count++;
         }
-        else if(s[i] == 'l' && l2_flag == 0 && l1_flag != 0) {
-            l2_flag = i;
+        else if(s[i] == 'l' && count == 3) {
+            count++;
         }
-        else if(s[i] == 'o' && o_flag == 0) {
-            o_flag = i;
+        else if(s[i] == 'o' && count == 4) {
+            count++;
         }
     }
-    printf("%d %d %d %d %d\n", h_flag, e_flag, l1_flag, l2_flag, o_flag);
-    if(h_flag < e_flag && e_flag < l1_flag && l1_flag < l2_flag && l2_flag < o_flag) {
+    if(count == 5) {
         printf("YES\n");
     }
     else {
